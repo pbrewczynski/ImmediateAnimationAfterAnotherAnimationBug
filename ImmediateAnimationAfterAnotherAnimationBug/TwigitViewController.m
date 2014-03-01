b85384f88a34384d4157424b7cba409a5417f336
Paweł Brewczyński <pbbluesm@gmail.com>
2014-03-02 00:02:32 +0100

    Initial commit

--- ImmediateAnimationAfterAnotherAnimationBug/TwigitViewController.m
+++ ImmediateAnimationAfterAnotherAnimationBug/TwigitViewController.m
@@ -10,6 +10,10 @@
 
 @interface TwigitViewController ()
 
+@property (strong, nonatomic) UIView *container;
+@property (strong, nonatomic) UIView *v1;
+@property (nonatomic)         NSInteger y;
+
 @end
 
 @implementation TwigitViewController
@@ -18,12 +22,38 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.y = 0;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)createV1View {
+    
+    self.container = [[UIView alloc] initWithFrame:CGRectMake(20, self.y, 80, 80)];
+    self.y += 85;  // Just so, next view will be underneath previous one
+    self.container.backgroundColor = [UIColor redColor];  // Background of container is RED.
+    
+    [self.view addSubview:self.container];
+}
+
+- (IBAction) performTransition {
+    
+    
+    self.v1  = [[UIView alloc] initWithFrame:self.container.bounds];
+    self.v1.backgroundColor = [UIColor greenColor];
+    
+    
+    [UIView transitionWithView:self.container duration:0.5 options:UIViewAnimationOptionTransitionFlipFromBottom animations:^{[self.container addSubview:self.v1];} completion:^(BOOL finished) {NSLog(@"I finished second animation");}];
+    
+}
+
+- (IBAction) createV1ViewANDperformTransition {
+    [self createV1View];
+    [self performTransition];
 }
 
 @end
